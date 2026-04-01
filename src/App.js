45a7b69cea8de8d0f84696a46713fd46b65d5298
Eucharia2124 <obiageristoehr2124@gmail.com>
2026-04-01 20:12:19 +0200

    install bootstrap and update the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
+      <div className="container">
       
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://github.com/Eucharia2124/weekfivereact" target="_blank" rel="noopener noreferrer">Obiageria</a>{" "} and is {" "}<a href="https://github.com/Eucharia2124/weekfivereact" target="_blank" rel="noopener noreferrer">open-source on GitHub</a>
+        {" "}and is {" "}hosted on <a href="https://weather-app-5-oby.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>. 
+      </footer>
+    </div>
     </div>
   );
+
 }
 
-export default App;
