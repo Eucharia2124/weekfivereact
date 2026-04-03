860009b92281c821a709934bafd9838dfd02cbaf
Eucharia2124 <obiageristoehr2124@gmail.com>
2026-04-03 22:47:00 +0200

    work on the weather css, js and App css

--- src/Weather.js
+++ src/Weather.js
@@ -7,30 +7,34 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city..." className="form-control" />
+                        <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary" />
+                        <input type="submit" value="Search" className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
             <h1>Lisbon</h1>
             <ul>
-                <li>Monday: Sunny, 25°C</li>
-                <li>Tuesday: Cloudy, 22°C</li>
-                <li>Wednesday: Rainy, 18°C</li>
+                <li>Monday: 07:00</li>
+                <li>Cloudy</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny" />
-                    25°C
+                    <div className="clearfix">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Cloudy" className="float-left" />
+                        <div className="float-left">
+                            <span className="temperature">25</span>
+                            <span className="unit">°C</span>
+                        </div>
+                    </div>
                 </div>
+    
                 <div className="col-6">
                     <ul>
                         <li> precipitation: 0%</li>
                         <li>Humidity: 48%</li>
                         <li>Wind: 23 km/h</li>
-                        <li>UV-Index: 0</li>
                     </ul>
                    
                 </div>
