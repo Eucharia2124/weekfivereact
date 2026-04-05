8a6f7c486a99fc8c6c388ee0b8184a219739bd30
Eucharia2124 <obiageristoehr2124@gmail.com>
2026-04-05 21:04:10 +0200

    updated

--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,51 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
-    const [weatherData, setWeatherData] = useState({});
-    const [ready, setReady] = useState(false);
+    const [weatherData, setWeatherData] = useState({ready: false});
+
     function handleResponse(response) {
         console.log(response.data);
+        
         setWeatherData({
-            temperature: response.data.main.temp,
-            wind: response.data.main.wind.speed,
+            ready: true,
+            temperature: response.data.temp,
+            date: new Date(response.data.dt * 1000),
+            wind: response.data.wind.speed, 
             humidity: response.data.main.humidity,
             description: response.data.weather[0].description,
             iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
             city: response.data.name,
-            });
+    });
+}
 
-        setReady(true);
-    }
-
-    if (ready) {
+    if (weatherData.ready) {
         return (
         <div className="Weather">
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
+                        <input 
+                        type="search" 
+                        placeholder="Enter a city..." 
+                        className="form-control" 
+                        autoFocus="on" />
                     </div>
                     <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary w-100" />
+                        <input type="submit" 
+                        value="Search" 
+                        className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
             <h1>{weatherData.city}</h1>
             <ul>
-                <li>Wednesday 07:00</li>
-                <li>{weatherData.description}</li>
+                <li>
+                    <FormattedDate date={weatherData.date} />
+                </li>
+                <li className="text-capitalize">{weatherData.description}</li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
