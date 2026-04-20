4007a69cdb9209e10fb02444f25248bdef273f27
Eucharia2124 <obiageristoehr2124@gmail.com>
2026-04-20 13:23:53 +0200

    updated

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecastDay.css";
+
 
 export default function WeatherForecastDay(props) {
     function maxTemperture() {
-        let temperature = Math.round(props.data.temperature.max);
+        let temperature = Math.round(props.data.temperature.maximum);
         return `${temperature}°`;
     }
 
     function minTemperture() {
-        let temperature = Math.round(props.data.temperature.min);
+        let temperature = Math.round(props.data.temperature.minimum);
         return `${temperature}°`;
     }
-
     function day() {
         let date = new Date(props.data.time * 1000);
         let day = date.getDay();
