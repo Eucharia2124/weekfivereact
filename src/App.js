451eaf454cafd8d0e8e7ed22b716120db9b7926f
Eucharia2124 <obiageristoehr2124@gmail.com>
2026-04-22 16:52:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <Weather defaultCity="Lisbon" />
       <footer>
         This project was coded by <a href="https://github.com/Eucharia2124/weekfivereact" target="_blank" rel="noopener noreferrer">Obiageria</a>{" "} and is {" "}<a href="https://github.com/Eucharia2124/weekfivereact" target="_blank" rel="noopener noreferrer">open-source on GitHub</a>
-        {" "}and is {" "}hosted on <a href="https://weather-app-5-oby.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>. 
+        {" "}and is {" "}hosted on <a href="https://week6projec.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>. 
       </footer>
     </div>
     </div>
